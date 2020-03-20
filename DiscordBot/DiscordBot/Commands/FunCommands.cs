ba47b074342106b70a3e76c685463c6a293e0ffb
Grant Shaw <40340717@live.napier.ac.uk>
2020-03-20 02:29:39 +0000

    Descriptions working.

--- DiscordBot/DiscordBot/Commands/FunCommands.cs
+++ DiscordBot/DiscordBot/Commands/FunCommands.cs
@@ -10,12 +10,24 @@ namespace DiscordBot
     class FunCommands : BaseCommandModule
     {
         [Command ("YouSmell")]
+        [Description ("Tells you that YOU SMELL")]
         public async Task YouSmell (CommandContext ctx)
         {
+           
             await ctx.Channel.SendMessageAsync("No you do!").ConfigureAwait(false);
 
         }
-        
+
+        [Command ("add")]
+        [Description("Adds two numbers")]
+        public async Task Add(CommandContext ctx, [Description ("The first number")] int numberOne, [Description("The second number")] int numberTwo)
+        {
+            await ctx.Channel
+                .SendMessageAsync((numberOne + numberTwo).ToString())
+                .ConfigureAwait(false);
+
+        }
+
 
 
 
